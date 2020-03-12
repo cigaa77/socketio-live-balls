5db5474235c5c867a2fd25d1bb2c4d08e349f3da
Ahmet <ahmetcilingir@outlook.com>
2020-03-12 12:53:55 +0300

    Aync-Await

--- public/javascripts/controllers/indexController.js
+++ public/javascripts/controllers/indexController.js
@@ -28,82 +28,84 @@ app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFacto
         })
     }
 
-    function initSocket(username) {
+    async function initSocket(username) {
         const connectionOptions = {
             reconnectionAttempts: 3,
             reconnectionDelay: 600,
+        };
+
+        try {
+            const socket = await indexFactory.connectSocket('http://localhost:3000', connectionOptions);
+
+            socket.emit('newUser', { username });
+            socket.on('initPlayers', (players) => {
+                $scope.players = players;
+                $scope.$apply();
+            })
+
+            socket.on('newUser', (data) => {
+                const messageData = {
+                    type: {
+                        code: 0,//  server or user message
+                        message: 0// connect or disconnect message
+                    },
+                    username: data.username
+                };
+                $scope.messages.push(messageData);
+                $scope.players[data.id] = data;
+                $scope.$apply();
+            })
+            socket.on('disUser', (user) => {
+                const messageData = {
+                    type: {
+                        code: 0,//  server or user message
+                        message: 1// connect or disconnect message
+                    },
+                    username: user.username
+                };
+                $scope.messages.push(messageData);
+                delete $scope.players[user.id]
+                $scope.$apply();
+            })
+            $scope.onClickPlayer = ($event) => {
+                console.log($event.offsetX, $event.offsetY);
+                let x = $event.offsetX, y = $event.offsetY;
+                socket.emit('animate', { x, y })
+                $('#' + socket.id).animate({ 'left': x, 'top': y })
+            }
+
+            socket.on('animate', (data) => {
+                $('#' + data.socketId).animate({ 'left': data.x, 'top': data.y })
+            })
+
+            $scope.newMessage = () => {
+                let message = $scope.message;
+                const messageData = {
+                    type: {
+                        code: 1
+                    },
+                    username: username,
+                    text: message
+                }
+
+                $scope.messages.push(messageData);
+                $scope.message = '';
+
+                socket.emit('newMessage', messageData);
+
+                showBubble(socket.id, message)
+                scrollTop();
+
+            }
+            socket.on('newMessage', (data) => {
+                $scope.messages.push(data);
+                $scope.$apply();
+                showBubble(data.socketId, data.text)
+                scrollTop();
+            })
+        } catch (error) {
+            console.log(error)
         }
 
-        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
-            .then((socket) => {
-                socket.emit('newUser', { username });
-                socket.on('initPlayers', (players) => {
-                    $scope.players = players;
-                    $scope.$apply();
-                })
-
-                socket.on('newUser', (data) => {
-                    const messageData = {
-                        type: {
-                            code: 0,//  server or user message
-                            message: 0// connect or disconnect message
-                        },
-                        username: data.username
-                    };
-                    $scope.messages.push(messageData);
-                    $scope.players[data.id] = data;
-                    $scope.$apply();
-                })
-                socket.on('disUser', (user) => {
-                    const messageData = {
-                        type: {
-                            code: 0,//  server or user message
-                            message: 1// connect or disconnect message
-                        },
-                        username: user.username
-                    };
-                    $scope.messages.push(messageData);
-                    delete $scope.players[user.id]
-                    $scope.$apply();
-                })
-                $scope.onClickPlayer = ($event) => {
-                    console.log($event.offsetX, $event.offsetY);
-                    let x = $event.offsetX, y = $event.offsetY;
-                    socket.emit('animate', { x, y })
-                    $('#' + socket.id).animate({ 'left': x, 'top': y })
-                }
-
-                socket.on('animate', (data) => {
-                    $('#' + data.socketId).animate({ 'left': data.x, 'top': data.y })
-                })
-
-                $scope.newMessage = () => {
-                    let message = $scope.message;
-                    const messageData = {
-                        type: {
-                            code: 1
-                        },
-                        username: username,
-                        text: message
-                    }
-
-                    $scope.messages.push(messageData);
-                    $scope.message = '';
-
-                    socket.emit('newMessage', messageData);
-
-                    showBubble(socket.id, message)
-                    scrollTop();
-
-                }
-                socket.on('newMessage', (data) => {
-                    $scope.messages.push(data);
-                    $scope.$apply();
-                    showBubble(data.socketId, data.text)
-                    scrollTop();
-                })
-            }).catch((err) => {
-                console.log(err);
-            })
     }
 }])
